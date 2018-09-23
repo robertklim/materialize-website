892eae8e18844568afa69f8efbae3208fa86e7d0
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-23 12:04:07 +0200

    Create photos grid

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var options = {}
     var instances = M.Sidenav.init(elems, options);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.materialboxed');
+    var options = {}
+    var instances = M.Materialbox.init(elems, options);
+});
