c3c60eddf871d508f3934d84d78d0de4dde67ac2
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-24 08:22:48 +0200

    Add parallax sections

--- js/main.js
+++ js/main.js
@@ -9,3 +9,9 @@ document.addEventListener('DOMContentLoaded', function () {
     var options = {}
     var instances = M.Materialbox.init(elems, options);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.parallax');
+    var options = {}
+    var instances = M.Parallax.init(elems, options);
+});
