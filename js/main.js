ff8f0ccc5261f7f9b3ffc90b022842d12bf84bb4
Robert Klim <kontakt.robert.klim@gmail.com>
2018-09-24 09:50:17 +0200

    Add contact form section

--- js/main.js
+++ js/main.js
@@ -21,3 +21,11 @@ document.addEventListener('DOMContentLoaded', function () {
     var options = {}
     var instances = M.Tabs.init(elems, options);
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.datepicker');
+    var options = {
+        disableWeekends: true
+    }
+    var instances = M.Datepicker.init(elems, options);
+});
